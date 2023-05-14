dc0021babfcb8008ed1ab970729ff52a6a27455a
alextanhongpin <alextan220990@gmail.com>
2023-05-14 16:17:36 +0800

    chore: update implementation

---
+++ app/otpsvc/README.md
@@ -0,0 +1,10 @@
+# OTP Service
+
+- documentation
+- reference
+- examples
+- tutorials
+
+
+- dependencies (bring your own dependencies)
+- diagram for flow and usecase
--- app/otpsvc/service.go
+++ app/otpsvc/service.go
@@ -25,7 +25,7 @@ type smsProvider interface {
 
 type cache interface {
 	Inc(ctx context.Context, key string) (int64, error)
-	Set(ctx context.Context, key string, ttl time.Duration) error
+	Set(ctx context.Context, key string, val string, ttl time.Duration) error
 	Get(ctx context.Context, key string) (string, error)
 	Del(ctx context.Context, key string) error
 }
@@ -33,22 +33,35 @@ type cache interface {
 type Service struct {
 	cache       cache
 	smsProvider smsProvider
+
+	// Domain specific identifier, e.g. withdrawal/payment.
+	// This is used to ensure no concurrent request is made.
+	domain string
+
+	// The specific template for this OTP message
+	template string
+}
+
+func New() *Service {
+	return &Service{
+		domain:   "unknown",
+		template: "OTP is %s",
+	}
 }
 
 type SendDto struct {
-	PhoneNumberE164 string // PhoneNumber in E164 format.
-	ExternalID      string // ExternalID to identify the request.
-	// Template?
+	PhoneNumber string // PhoneNumber in E164 format.
+	ExternalID  string // ExternalID to identify the request. You can for example hash the request.
 }
 
 func (s *Service) Send(ctx context.Context, dto SendDto) error {
-	phone := domain.PhoneNumber(dto.PhoneNumberE164)
+	phone := domain.PhoneNumber(dto.PhoneNumber)
 	if err := phone.Validate(); err != nil {
-		return fmt.Errorf("%w: %q", err, dto.PhoneNumberE164)
+		return fmt.Errorf("%w: %q", err, dto.PhoneNumber)
 	}
 
 	// - Cooldown? Skip
-	if s.isCooldown(ctx, dto.ExternalID) {
+	if s.isCooldown(ctx, phone.String()) {
 		return ErrCooldown
 	}
 
@@ -62,59 +75,74 @@ func (s *Service) Send(ctx context.Context, dto SendDto) error {
 	// - count > 5?  Set cooldown = 1h
 	// - count > 3? Set cooldown = 10m
 	// - set cooldown = 1m
-	cooldownDuration := cooldownDurationByCount(count)
-	if err := s.cache.Set(ctx, cooldownKey(dto.ExternalID), cooldownDuration); err != nil {
-		return err
+	{
+		key := s.cooldownKey(phone.String())
+		val := dto.ExternalID
+		ttl := cooldownDurationByCount(count)
+		if err := s.cache.Set(ctx, key, val, ttl); err != nil {
+			return err
+		}
 	}
 
 	otp := NewOTP()
 
 	// Save OTP in cache with TTL 3m
-	otpDuration := 3 * time.Minute
-	if err := s.cache.Set(ctx, otpKey(dto.ExternalID, otp), otpDuration); err != nil {
-		return err
+	{
+		key := s.otpKey(phone.String(), otp)
+		val := dto.ExternalID
+		ttl := 3 * time.Minute
+		if err := s.cache.Set(ctx, key, val, ttl); err != nil {
+			return err
+		}
 	}
 
 	// Send OTP
-	return s.smsProvider.Send(ctx, phone.String(), fmt.Sprintf("OTP is %s", otp))
+	return s.smsProvider.Send(ctx, phone.String(), fmt.Sprintf(s.template, otp))
 }
 
 type VerifyDto struct {
-	OTP        string
-	ExternalID string
+	PhoneNumber string
+	OTP         string
 }
 
-func (s *Service) Verify(ctx context.Context, dto VerifyDto) error {
-	_, err := s.cache.Get(ctx, otpKey(dto.ExternalID, dto.OTP))
+// Verify verifies the phone number and OTP, and returns the associated
+// external id.
+func (s *Service) Verify(ctx context.Context, dto VerifyDto) (string, error) {
+	phone := domain.PhoneNumber(dto.PhoneNumber)
+	if err := phone.Validate(); err != nil {
+		return "", fmt.Errorf("%w: %q", err, dto.PhoneNumber)
+	}
+
+	externalID, err := s.cache.Get(ctx, s.otpKey(phone.String(), dto.OTP))
 	if errors.Is(err, ErrKeyNotFound) {
-		return ErrOTPNotFound
+		return "", ErrOTPNotFound
 	}
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	if err := s.cache.Del(ctx, otpKey(dto.ExternalID, dto.OTP)); err != nil {
-		return err
+	if err := s.cache.Del(ctx, s.otpKey(phone.String(), dto.OTP)); err != nil {
+		return "", err
 	}
 
-	if err := s.cache.Del(ctx, cooldownKey(dto.ExternalID)); err != nil {
-		return err
+	if err := s.cache.Del(ctx, s.cooldownKey(phone.String())); err != nil {
+		return "", err
 	}
 
-	return nil
+	return externalID, nil
 }
 
-func (s *Service) isCooldown(ctx context.Context, id string) bool {
-	_, err := s.cache.Get(ctx, cooldownKey(id))
+func (s *Service) isCooldown(ctx context.Context, phoneNumber string) bool {
+	_, err := s.cache.Get(ctx, s.cooldownKey(phoneNumber))
 	return !errors.Is(err, ErrKeyNotFound)
 }
 
-func otpKey(id, otp string) string {
-	return fmt.Sprintf("otp:%s:%s", id, otp)
+func (s *Service) otpKey(phoneNumber, otp string) string {
+	return fmt.Sprintf("otp:%s:pn:%s:otp:%s", s.domain, phoneNumber, otp)
 }
 
-func cooldownKey(id string) string {
-	return fmt.Sprintf("otp:cooldown:%s", id)
+func (s *Service) cooldownKey(phoneNumber string) string {
+	return fmt.Sprintf("otp:%s:pn:%s:cooldown", s.domain, phoneNumber)
 }
 
 func cooldownDurationByCount(count int64) time.Duration {
