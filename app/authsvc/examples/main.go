bdd920f51b95646529f09d4cf6050c0526f617b6
alextanhongpin <alextan220990@gmail.com>
2023-05-20 16:15:29 +0800

    chore: extend examples with additional steps

--- app/authsvc/examples/main.go
+++ app/authsvc/examples/main.go
@@ -17,30 +17,44 @@ type User struct {
 	Email string
 }
 
-type loginService[T any] interface {
-	Login(ctx context.Context, dto authsvc.LoginDto) (*T, error)
+type AuthUsecase struct {
+	// Method steps
+	loginSteps interface {
+		Login(ctx context.Context, dto authsvc.LoginDto) (*User, error)
+		GenerateToken(ctx context.Context, user *User) (string, error)
+	}
+	// anotherMethodSteps ..
 }
 
-type AuthUsecase struct {
-	loginService loginService[User]
+type loginSteps struct {
+	*authsvc.LoginService[User]
+	*generateTokenStep
 }
 
 type LoginDto = authsvc.LoginDto
 
-func (uc *AuthUsecase) Login(ctx context.Context, dto LoginDto) (*User, error) {
+func (uc *AuthUsecase) Login(ctx context.Context, dto LoginDto) (string, error) {
 	defer func(start time.Time) {
 		log.Println("took: ", time.Since(start))
 	}(time.Now())
 
 	// Add other steps, like monitoring etc.
-	return uc.loginService.Login(ctx, dto)
+	user, err := uc.loginSteps.Login(ctx, dto)
+	if err != nil {
+		return "", err
+	}
+
+	return uc.loginSteps.GenerateToken(ctx, user)
 }
 
 func main() {
 	svc := authsvc.NewLoginService[User](&repository{})
 	ctx := context.Background()
 
-	uc := &AuthUsecase{loginService: svc}
+	uc := &AuthUsecase{loginSteps: &loginSteps{
+		LoginService:      svc,
+		generateTokenStep: &generateTokenStep{},
+	}}
 	user, err := uc.Login(ctx, LoginDto{
 		Email:    "john.doe@mail.com",
 		Password: "12345678",
@@ -68,4 +82,11 @@ func (r *repository) FindUserByEmail(ctx context.Context, email string) (*authsv
 			Email: "john.doe@mail.com",
 		},
 	}, nil
+}
+
+type generateTokenStep struct {
+}
+
+func (s *generateTokenStep) GenerateToken(ctx context.Context, u *User) (string, error) {
+	return u.ID.String(), nil
 }
